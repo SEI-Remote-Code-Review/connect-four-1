5823fcea246a44169ecb6984f0449c88026ef92c
jruiz <jruiz.wk@gmail.com>
2022-02-17 23:48:26 -0600

    add checkDarkPref

--- js/app.js
+++ js/app.js
@@ -162,3 +162,12 @@ function render(){
 function toggleLightDark() {
   body.className = body.className === "dark" ? "" : "dark"
 }
+
+function checkDarkPref() {
+  if (
+    window.matchMedia("(prefers-color-scheme:dark)").matches &&
+    body.className !== "dark"
+  ) {
+    toggleLightDark()
+  }
+}
